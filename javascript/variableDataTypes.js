7834d9812e1ef8cd9f746afe26b021626d4a6bb8
AnitaPK <anita.khamitkar@gmail.com>
2026-07-14 09:55:10 +0530

    data types

---
+++ javascript/variableDataTypes.js
@@ -0,0 +1,13 @@
+let vaishnaviJ = Symbol('Vaishanvi')
+
+let vaishnaviS = Symbol('Vaishanavi')
+
+const sNameJ = 'Vaishanvi'
+const sNameS = 'Vaishanvi'
+
+console.log(vaishnaviJ == vaishnaviS)
+console.log(sNameJ == sNameS)
+
+x = 5 
+y = 5
+console.log(x == y)
