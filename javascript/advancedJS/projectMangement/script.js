138c20156afb929fc5b9cf7e61187c1b6d8ebf66
AnitaPK <anita.khamitkar@gmail.com>
2026-09-04 11:38:00 +0530

    project mgmt - II

--- javascript/advancedJS/projectMangement/script.js
+++ javascript/advancedJS/projectMangement/script.js
@@ -2,7 +2,8 @@ let currentPage = 1;
 const numberOfProjects = 10;
 // This will store all projects received from API 
 let allProjects = [];
-
+let departments = []
+let departmentsArray = []
 
 function renderProjects(projects) {
 
@@ -65,10 +66,28 @@ function renderProjects(projects) {
         e.preventDefault();
         const page = Number(e.target.dataset.page);
         if (page >= 1 && page <= totalPages && page !== currentPage) {
-            currentPage = page; renderProjects(projects);
+            currentPage = page;
+            renderProjects(projects);
         }
     };
 }
+
+function SearchByTitle() {
+    searchKey = document.querySelector("#searchProject").value
+    console.log(searchKey)
+    // add condition ig searchKey is empty 
+    projectsListBySearch = allProjects.filter(proj => {
+        return proj.ProjectName.toLowerCase().includes(searchKey.toLowerCase())
+    })
+    console.log(projectsListBySearch)
+
+    renderProjects(projectsListBySearch)
+}
+
+
+
+
+
 async function fetchData() {
     await fetch("https://674e84f1635bad45618eebc1.mockapi.io/api/v1/projects")
         .then(res => res.json())
@@ -76,14 +95,37 @@ async function fetchData() {
             allProjects = data;
 
             currentPage = 1;
+            departments = new Set(data.map(p => {
+                // return [...new Set(p.department)]
+                // return Array.from(new Set(p.department))
+                return p.Department
+            }))
+            console.log(departments)
+            departmentsArray = [...departments]
+            console.log(departmentsArray)
+            document.querySelector("#deptRender").innerHTML = `
+        <select id="departmentFilter" class="form-select">
+        <option value="">All Departments</option>
+            ${departmentsArray.map(d => `
+            <option value=${d}>${d}</option>
+            `).join("")}
+        </select>
+`;
+
+            document.querySelector("#departmentFilter").addEventListener("change", () => {
+                deptKey = document.querySelector("#departmentFilter").value
+                projectsByDeptKey = allProjects.filter((proj) => {
+                    // add condition for select All projects 
+                    return proj.Department == deptKey
+                })
+                renderProjects(projectsByDeptKey)
+            })
+            // code for filter by priority 
+            // code for filter by status 
             renderProjects(data)
         })
         .catch(error => console.log(error))
 }
 
-
-
-
-
-
+// show loader while fetching data 
 window.addEventListener("load", fetchData)
